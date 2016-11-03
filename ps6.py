bfc83e5b8e17e7687feb5fe9d031fe08b48f3138
yubi <yubi@foxmail.com>
2016-11-04 00:16:59 +0800

    fix bugs: subclass(self,xxx) dict.copy()

--- ps6.py
+++ ps6.py
@@ -148,10 +148,10 @@ class PlaintextMessage(Message):
         code is repeated
         '''
         #pass #delete this line and replace with your code here
-        Message.__init__(text)
+        Message.__init__(self, text)
         self.shift = shift
-        self.encrypting_dict = Message.build_shift_dict(shift)
-        self.message_text_encrypted = Message.apply_shift(shift)
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
 
     def get_shift(self):
         '''
@@ -167,7 +167,7 @@ class PlaintextMessage(Message):
         
         Returns: a COPY of self.encrypting_dict
         '''
-        return self.encrypting_dict
+        return self.encrypting_dict.copy()
 
     def get_message_text_encrypted(self):
         '''
@@ -188,9 +188,9 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        self.__init__(self.text, shift)
+        self.__init__(self.message_text, shift)
 
-
+        
 class CiphertextMessage(Message):
     def __init__(self, text):
         '''
