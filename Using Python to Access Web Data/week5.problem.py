9ee444e5843f2a737f863b338d6e63ff773b0e97
yubi <yubi@foxmail.com>
2016-11-10 22:17:17 +0800

    change problem by regular expression

--- Using Python to Access Web Data/week5.problem.py
+++ Using Python to Access Web Data/week5.problem.py
@@ -7,6 +7,7 @@ Created on Thu Nov 10 11:05:59 2016
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
 sampleData = 'http://python-data.dr-chuck.net/comments_42.xml'
 actualData = 'http://python-data.dr-chuck.net/comments_303859.xml'
@@ -17,14 +18,18 @@ try:
     #tags = soup.find_all('count')
     #print(tags)
     #print(sum(int(tag.string) for tag in tags))
+    results = re.findall('<count>([0-9]+)</count>', url.read().decode('utf-8'))
+    print(sum(map(int, results)))
+    '''
     tree = ET.fromstring(url.read().decode('utf-8'))
+    results = tree.findall('.//count')
+    print(sum(int(item.text) for item in results))
+    '''
     '''
     results = tree.findall('.//count')
     for item in results:
         print(type(item.text))
     '''
-    results = tree.findall('.//count')
-    print(sum(int(item.text) for item in results))
 
 except urllib.request.HTTPError as error:
     print(error)
