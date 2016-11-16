c488542ce765e2115ea3ba075cd28fb65cf3a447
yubi <yubi@foxmail.com>
2016-11-16 15:27:50 +0800

    Examples for The Series Data Structure

--- Introduction_to_Data_Science_in_Python/week2/examples_The_Series_Data_Structure.py
+++ Introduction_to_Data_Science_in_Python/week2/examples_The_Series_Data_Structure.py
@@ -17,3 +17,70 @@ print(pd.Series(animals))
 #%%
 numbers = [1, 2, 3]
 print(pd.Series(numbers))
+
+#%%
+animals = ['Tiger', 'Bear', None]
+print(pd.Series(animals))
+
+#%%
+numbers = [1, 2, None]
+print(pd.Series(numbers))
+
+#%%
+import numpy as np
+print(np.nan == None)
+print(np.nan == np.nan)
+
+#%%
+print(np.isnan(np.nan))
+#print(np.isnan(animals[-1]))  TypeError
+
+#%%
+sports = {'Archery': 'Bhutan',
+          'Golf': 'Scotland',
+          'Sumo': 'Japan',
+          'Taekwonodo': 'South Korea'}
+s = pd.Series(sports)
+print(s)
+print(s.index)
+print(s['Golf'])
+#print(s['Scotland']) TypeError
+
+#%%
+s = pd.Series(['Tiger', 'Bear', 'Moose'], index=['India', 'America', 'Canada'])
+print(s['India'])
+#print(s['Tiger']) TypeError
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
