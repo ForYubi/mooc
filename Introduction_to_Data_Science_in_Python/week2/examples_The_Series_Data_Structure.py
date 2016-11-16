e0aeab0bd1a877d3a27e08397a7d7a2b9b0182e6
yubi <yubi@foxmail.com>
2016-11-16 15:09:26 +0800

    Introduction_to_Data_Science_in_Python Initialize

---
+++ Introduction_to_Data_Science_in_Python/week2/examples_The_Series_Data_Structure.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 15:01:24 2016
+
+@author: Administrator
+"""
+
+import pandas as pd
+
+#%%
+#pd.Series?
+
+#%%
+animals = ['Tiger', 'Bear', 'Moose']
+print(pd.Series(animals))
+
+#%%
+numbers = [1, 2, 3]
+print(pd.Series(numbers))
